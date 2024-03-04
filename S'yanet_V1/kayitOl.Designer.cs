e938768d1df53134ad14708c820087f024974224
mervadogan <142056195+mervadogan@users.noreply.github.com>
2024-03-04 14:00:52 +0300

    güncelleme

--- S'yanet_V1/kayitOl.Designer.cs
+++ S'yanet_V1/kayitOl.Designer.cs
@@ -204,11 +204,11 @@
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Impact", 45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(34, 61);
+            this.label5.Location = new System.Drawing.Point(12, 49);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(348, 75);
+            this.label5.Size = new System.Drawing.Size(380, 75);
             this.label5.TabIndex = 2;
-            this.label5.Text = "CyberSweep";
+            this.label5.Text = "CyberSweepp";
             // 
             // kayitOl
             // 
